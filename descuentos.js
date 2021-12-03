9d3d797d93007eabfb63edb0aec7470b14441e53
lucascarpii <lucascarpii2002@gmail.com>
2021-12-03 19:13:55 -0300

    Los cupones ya son funcionales! Taller#2 terminado

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,5 @@
-const precioOriginal = 120;
-const descuento = 18;
+// const precioOriginal = 120;
+// const descuento = 18;
 
 // console.log({
 //   precioOriginal,
@@ -8,30 +8,69 @@ const descuento = 18;
 //   precioConDescuento,
 // });
 
-function calcularPrecioConDescuento(precio, descuento) {
+function calcularPrecioConDescuento(precio, descuento, cupon) {
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
-  console.log(
-    "El precio original es: $" +
-      precio +
-      " y con el descuento aplicado queda en: $" +
-      precioConDescuento
-  );
+  // console.log(
+  //   "El precio original es: $" +
+  //     precio +
+  //     " y con el descuento aplicado queda en: $" +
+  //     precioConDescuento
+  // );
 
   return precioConDescuento;
 }
 
+const coupons = [
+  {
+    name: "blackfRID4Y",
+    discount: 15,
+  },
+  {
+    name: "D5couN7S",
+    discount: 30,
+  },
+  {
+    name: "4notheRDIscnt",
+    discount: 25,
+  },
+];
+
 function onClickButtonPriceDiscount() {
+  //Datos descuento estandar
   const inputPrice = Number(document.getElementById("InputPrice").value);
   const inputDiscount = Number(document.getElementById("InputDiscount").value);
   const parrafo = document.getElementById("finalPrice");
-  const finalPrice = calcularPrecioConDescuento(inputPrice, inputDiscount);
 
-  if (inputPrice > 0) {
-    parrafo.innerText = "El precio con descuento es de $" + finalPrice;
+  //Datos cupones
+  const inputCoupon = document.getElementById("InputCupon");
+  const couponValue = inputCoupon.value;
+  // Funcion en la que validamos los cupones
+  const isCouponValueValid = function (coupon) {
+    return coupon.name === couponValue;
+  };
+  //Resultado de la validacion previa
+  const userCoupon = coupons.find(isCouponValueValid);
+
+  // Guardamos el del precio final sin el cupon agregado
+  const prevFinalPrice = calcularPrecioConDescuento(inputPrice, inputDiscount);
+
+  if (couponValue === "") {
+    if (inputPrice > 0) {
+      parrafo.innerText = "El precio con descuento es de $" + prevFinalPrice;
+    } else {
+      parrafo.innerText = "No hay valores a calcular";
+    }
   } else {
-    parrafo.innerText = "No hay valores a calcular";
+    if (!userCoupon) {
+      const invalidCupon = document.getElementById("invalidCupon");
+      invalidCupon.innerText = "El cupón no es válido";
+    } else {
+      const descuento = userCoupon.discount;
+      const finalPrice = calcularPrecioConDescuento(prevFinalPrice, descuento);
+      parrafo.innerText = "El precio con descuento son: $" + finalPrice;
+      invalidCupon.innerText = "";
+    }
   }
 }
-
